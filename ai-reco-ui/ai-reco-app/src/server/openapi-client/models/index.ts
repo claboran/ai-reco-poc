6b1f46658cfe385b070a36f8a8d2e175045026b6
Christian Laboranowitsch <christian.laboranowitsch@extern.cognizant-mobility.com>
2025-09-01 07:46:59 +0200

    feat(chat): New API for BFF generated

--- ai-reco-ui/ai-reco-app/src/server/openapi-client/models/index.ts
+++ ai-reco-ui/ai-reco-app/src/server/openapi-client/models/index.ts
@@ -1,4 +1,7 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './ChatMessage';
+export * from './ChatRequest';
+export * from './ChatResponse';
 export * from './Product';
 export * from './SearchRequest';
